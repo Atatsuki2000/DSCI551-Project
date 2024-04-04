31044d038398134b94301e322c223c6a863c8bce
Atatsuki2000 <sammylee20001021@gmail.com>
2024-04-04 16:08:19 -0700

    Delete unused files

--- Create_initial_databases.py
+++
@@ -1,4 +0,0 @@
-import Database
-
-Database.create_database("nba_0")
-Database.create_database("nba_1")
--- Insert_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import Database
-
-df = pd.read_csv("./Data/all_team_game_stats.csv")
-
-Database.insert_data(df, 0)
-Database.insert_data(df, 1)
